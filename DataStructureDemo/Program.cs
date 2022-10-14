705081470393833be60ce9c0a795ced80107fdeb
pareeksachin77 <pareeksachin77@gmail.com>
2022-10-14 23:02:03 +0530

    day14 uploaded

---
+++ DataStructureDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using DataStructureDemo;
+Console.WriteLine("Hello World!");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.AddAll(23, 56, 78, 34, 56);
+//list.InsertAtParticularPosition(2,30);
+list.Display();
+Console.ReadKey();
